20d0d360fdb97d94c280e58444de919d1b88845c
Arne Meier <arne@loremipsum.ee>
2021-12-07 21:16:01 +0200

    Ex5 ok

--- Ruler/Line.php
+++ Ruler/Line.php
@@ -8,6 +8,10 @@ class Line
     public $y1;
     public $x2;
     public $y2;
+    public $deltaX;
+    public $dirX;
+    public $deltaY;
+    public $dirY;
     public static $id = 1;
 
     public function __construct(string $input)
@@ -16,15 +20,18 @@ class Line
         $firstPair = preg_split('/\,/', $pairs[0]);
         $secondPair = preg_split('/\,/', $pairs[1]);
 
-        //constraint for hor/ver only
-        if ($firstPair[0] == $secondPair[0] || $firstPair[1] == $secondPair[1]) {
-            $this->x1 = $firstPair[0];
-            $this->y1 = $firstPair[1];
-            $this->x2 = $secondPair[0];
-            $this->y2 = $secondPair[1];
+        $this->x1 = (int)$firstPair[0];
+        $this->y1 = (int)$firstPair[1];
+        $this->x2 = (int)$secondPair[0];
+        $this->y2 = (int)$secondPair[1];
 
-            $this->id = self::$id++;
-        }
+        $this->deltaX = $this->x2 - $this->x1;
+        $this->deltaY = $this->y2 - $this->y1;
+
+        $this->dirX = $this->deltaX == 0 ? 0 : $this->deltaX / abs($this->deltaX);
+        $this->dirY = $this->deltaY == 0 ? 0 : $this->deltaY / abs($this->deltaY);
+
+        $this->id = self::$id++;
 
     }
 }
